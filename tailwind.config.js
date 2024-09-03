01ca8d096f02ad73cda5b7fdfc1fc0d971c24d48
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-09-03 12:54:55 +0900

    設定変更。今までが間違っていた。。。

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      main: "#4cb3f8",
-      text: {
-        regular: "#333333",
-        light: "#4d4d4d",
+    extend: {
+      colors: {
+        main: "#4cb3f8",
+        text: {
+          regular: "#333333",
+          light: "#4d4d4d",
+        },
+        button: {
+          primary: "#4cb3f8",
+          normal: "#b3b3b3",
+        },
+        bg: {
+          light: "#f5f8fa",
+          dark: "#c8e6fa",
+        },
       },
-      button: {
-        primary: "#4cb3f8",
-        normal: "#b3b3b3",
+      fontFamily: {
+        main: ["Noto Sans JP"],
+        serviceName: ["Gotham Bold"],
       },
-      bg: {
-        light: "#f5f8fa",
-        dark: "#c8e6fa",
-      },
-    },
-    fontFamily: {
-      main: ["Noto Sans JP"],
-      serviceName: ["Gotham Bold"],
     },
   },
   plugins: [],
